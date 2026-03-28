7779a7cbfb879638c0a8f31950d041510aeee048
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-03-27 23:32:27 -0300

    feat: implementar serializer para cadastro de admins

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,3 +1,4 @@
+from allauth.account.adapter import get_adapter
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from dj_rest_auth.serializers import LoginSerializer, UserDetailsSerializer
 from rest_framework import serializers
@@ -16,15 +17,33 @@ class CustomUserDetailsSerializer(UserDetailsSerializer):
         read_only_fields = fields
 
 
-# serializer para cadastro de usuários pelo admin
 class CustomRegisterSerializer(RegisterSerializer):
     username = None
     email = serializers.EmailField(required=True)
     password1 = serializers.CharField(write_only=True, style={"input_type": "password"})
     password2 = serializers.CharField(write_only=True, style={"input_type": "password"})
 
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data.update(
+            {
+                "role": User.RoleChoices.ADMIN,
+                "is_staff": True,
+                "is_superuser": True,
+            }
+        )
+        return data
+
     def save(self, request):
-        user = super().save(request)
-        user.role = User.RoleChoices.RESPONDENT
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+
+        user = adapter.save_user(request, user, self, commit=False)
+        user.role = self.cleaned_data.get("role")
+        user.is_staff = self.cleaned_data.get("is_staff")
+        user.is_superuser = self.cleaned_data.get("is_superuser")
         user.save()
+
+        self.custom_signup(request, user)
         return user
