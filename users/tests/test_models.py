eaa29e42bc4644b2a98df4848dbea48593fd7636
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-03-31 20:22:31 -0300

    refactor: mover permissões e modelos abstratos para core

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,21 +1,9 @@
 import pytest
 
-from users.models import UUIDPrimaryKeyModel
 from users.tests.factories import UserFactory
 
 
-class FakeModel(UUIDPrimaryKeyModel):
-    class Meta:
-        app_label = "users_tests"
-
-
 @pytest.mark.django_db
-def test_uuid_primary_key_model_to_str():
-    instance = FakeModel()
-    assert str(instance) == str(instance.id)
-
-
-@pytest.mark.django_db
-def test_user_factory_to_str():
+def test_user_to_str():
     user = UserFactory()
     assert str(user) == str(user.email)
