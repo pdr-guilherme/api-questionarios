5bf600087a1ab5d0124259a6efdee76849cff168
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-04-04 15:09:59 -0300

    fix: corrigir erro de digitação

--- core/models/base.py
+++ core/models/base.py
@@ -16,7 +16,7 @@ class UUIDPrimaryKeyModel(models.Model):
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(_("created at"), auto_now_add=True)
-    updated_at = models.DateTimeField(_("edited at"), auto_now=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
 
     class Meta:
         abstract = True
