1583cb48db5dd2108c7689a9c8fbe85ffe23d108
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-04-04 22:41:09 -0300

    feat: adicionar modelos restantes ao site de admin

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from surveys.models import Question, Survey
+from surveys.models import Option, Question, QuestionImage, Survey
 
 admin.site.register(Survey)
 admin.site.register(Question)
+admin.site.register(QuestionImage)
+admin.site.register(Option)
