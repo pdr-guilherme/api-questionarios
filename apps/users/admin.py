04e5f9004a99d2506f8b1838e41a3687f3a8b967
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-05-14 21:34:51 -0300

    feat: adicionar campo created_by ao modelo administrativo de usuário

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -20,7 +20,7 @@ class CustomUserAdmin(UserAdmin):
         "is_active",
     )
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (None, {"fields": ("email", "password", "created_by")}),
         (
             "Permissions",
             {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
