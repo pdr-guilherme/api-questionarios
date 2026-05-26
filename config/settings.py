065ca79a4d8810c4ca11fdc322346e446f09c189
Pedro Guilherme <pedro.guilherme.dsa@gmail.com>
2026-05-26 15:23:09 -0300

    chore: configurar origens confiáveis para csrf

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 # Application definition
 
 THIRD_PARTY_APPS = [
